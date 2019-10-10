1d83ecc1fc5388ecc5d5d1078515f08d0adbd827
ambiwakchaure <ambiwakchaure.abw@gmail.com>
2019-10-10 13:57:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/volleytutorial/string_request/StringRequestActivity.java
@@ -0,0 +1,16 @@
+package com.example.volleytutorial.string_request;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.volleytutorial.R;
+
+public class StringRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
